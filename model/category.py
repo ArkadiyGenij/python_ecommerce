a15c78032e8e9e76ec601cc3c2f7f786cffad4e5
Arkadiy <agongadze8@gmail.com>
2024-03-05 00:31:21 +0300

    upgrade project

--- model/category.py
+++ model/category.py
@@ -3,13 +3,16 @@ class Category:
     description: str
     products: list
 
-    category_count = 0
-    unique_product = 0
+    total_categories = 0
+    total_unique_product = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products
+
+        Category.total_categories += 1
+        Category.total_unique_product += len(products)
 
     def __str__(self):
         return f"{self.name} - {self.description}"
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,11 +1,26 @@
 import pytest
 
 from model.category import Category
+from model.product import Product
 
 
 @pytest.fixture()
 def category():
     return Category('Еда', 'Продукты питания на каждый день', ['Морковь', 'Лук', 'Гречка'])
+
+
+@pytest.fixture()
+def electronics_category():
+    iphone = Product("iPhone", "Смартфон", 1000, 50)
+    macbook = Product("MacBook", "Ноутбук", 1500, 30)
+    return Category("Электроника", "Технические устройства", [iphone, macbook])
+
+
+@pytest.fixture()
+def clothing_category():
+    tshirt = Product("Футболка", "Футболка с логотипом", 20, 100)
+    jeans = Product("Джинсы", "Джинсы стандартной длины", 30, 80)
+    return Category("Одежда", "Одежда и аксессуары", [tshirt, jeans])
 
 
 def test_category_init(category):
@@ -14,14 +29,9 @@ def test_category_init(category):
     assert category.products == ['Морковь', 'Лук', 'Гречка']
 
 
-def test_count_products(category):
-    assert len(category.products) == 3
+def test_category_total_count(electronics_category, clothing_category):
+    assert Category.total_categories == 2
 
 
-def test_count_categories():
-    assert Category.category_count == 0
-
-
-def test_unique_products(category):
-    unique_products = set(category.products)
-    assert len(unique_products) == len(category.products)
+def test_unique_product_count(electronics_category, clothing_category):
+    assert Category.total_unique_product == 4
